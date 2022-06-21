0ccf0800e47d94b80056c7da8b046bdaef71162d
majate <majate@kth.se>
2022-06-21 15:36:36 +0200

    install materail UI

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
-const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+const { merge } = require('webpack-merge')
+const singleSpaDefaults = require('webpack-config-single-spa-react-ts')
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
-    orgName: "exjobb-mfa",
-    projectName: "header",
+    orgName: 'exjobb-mfa',
+    projectName: 'header',
     webpackConfigEnv,
     argv,
-  });
+  })
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-  });
-};
+    externals: ['@mui/material'],
+  })
+}
